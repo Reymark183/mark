765b299f725fc0b11a101befd26225c294eb8da5
Reymark183 <remarkjurillayacap@gmail.com>
2025-05-20 23:54:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text", {
-    strings:["Basketball player sometimes", "Tiktokerist", "Humble ferson", "Multi talented (eme)",],
+    strings:["Basketball player sometimes", "Handsome sabi ni mama", "Humble ferson", "Multi talented (eme)",],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
